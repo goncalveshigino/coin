ccb392e63b5e1ee27fc14edc17577b74088e3279
goncalveshigino <goncalveshigino20@gmail.com>
2023-09-15 12:24:26 +0100

    Initial Commit

---
+++ Coins/CoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsApp.swift
+//  Coins
+//
+//  Created by Goncalves Higino on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
